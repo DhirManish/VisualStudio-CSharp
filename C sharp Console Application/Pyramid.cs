f1e168ef08ec69d68d64c80b2e45ba60ef003851
DhirManish <manish.dhir4@gmail.com>
2017-12-27 22:27:35 +0530

    Right Isosceles Pyramid

---
+++ C sharp Console Application/Pyramid.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String s = "*";
+
+            for (int i = 1; i <= 15; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(s);
+                }
+                Console.Write("\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
