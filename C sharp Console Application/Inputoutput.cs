9d0670e1dc38786877b65c01a0764dcc052412d4
DhirManish <manish.dhir4@gmail.com>
2017-12-27 22:39:58 +0530

    Input the values using for loop within list and print using foreach

---
+++ C sharp Console Application/Inputoutput.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int parameter = 5;
+
+            List<String> val = new List<string>();
+
+            for (int i=1; i <= parameter; i++)
+            {
+               
+                val.Add(Console.ReadLine());
+
+            }
+
+            foreach(String values in val)
+            {
+                Console.Write(values);
+            }
+            
+
+            Console.ReadLine();
+        }
+    }
+}
