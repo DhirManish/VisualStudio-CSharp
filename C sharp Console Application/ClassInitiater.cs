98db1da8712634792440fa04249ceb8679b731a3
DhirManish <manish.dhir4@gmail.com>
2017-12-29 00:44:03 +0530

    Inherit the data member of parent class using member functions

---
+++ C sharp Console Application/ClassInitiater.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClassInitiater
+{
+
+    class ClassInitater
+    {
+
+        string s = "Coder";
+
+        public string returnname ()
+        {
+            return s;
+        }
+
+    }
+
+    class Program : ClassInitater
+    {
+        static void Main(string[] args)
+        {
+
+            ClassInitater CI = new ClassInitater();
+
+            Console.Write(CI.returnname());
+
+        }
+    }
+}
