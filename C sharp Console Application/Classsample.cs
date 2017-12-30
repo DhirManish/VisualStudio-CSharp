8d6f99267d870ad2e4dbf17c09c0caae1756613b
DhirManish <manish.dhir4@gmail.com>
2017-12-30 18:03:07 +0530

    Instatiate class object in main method

---
+++ C sharp Console Application/Classsample.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+  
+
+    class Classsample 
+    {
+
+        public string value()
+        {
+
+            String s = Console.ReadLine();
+            return s;
+
+        }
+
+        static void Main(string[] args)
+        {
+            Classsample cs = new Classsample();
+
+            Console.Write(cs.value());
+            Console.Read();
+
+           
+        }
+    }
+}
