6bf3546a4e09b7374e2c3ebb803afed37036c0a5
DhirManish <manish.dhir4@gmail.com>
2017-12-31 21:59:05 +0530

    Calculate factorial of number using for loop

---
+++ C sharp Console Application/Factorial.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code
+{
+    class Factorial
+    {
+        static void Main(string[] args)
+        {
+
+            int number = int.Parse(Console.ReadLine());
+            int total = number;
+
+            for(int i=1; i<= number; i++)
+            {
+                total = total * (number - 1);
+
+                number = number - 1;
+
+            }
+
+            Console.Write("Factotial of number is " + total);
+
+            Console.ReadLine();
+
+        }
+    }
+}
