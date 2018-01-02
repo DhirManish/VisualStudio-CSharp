ca572eb630ea9dee4c2d5b08c15ec276cae3fb8b
DhirManish <manish.dhir4@gmail.com>
2018-01-02 22:52:44 +0530

    First commit of 2018

---
+++ C sharp Console Application/NewYearGreetings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class NewYEarGreetings
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Happy New Year:)");
+
+            Console.ReadLine();
+
+        }
+    }
+}
