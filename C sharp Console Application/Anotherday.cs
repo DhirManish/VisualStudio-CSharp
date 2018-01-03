edb18a213c2c41d1d155f0a8a2b88f9d7cfcb85c
DhirManish <manish.dhir4@gmail.com>
2018-01-03 23:24:32 +0530

    12 X 12

---
+++ C sharp Console Application/Anotherday.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Anotherday
+    {
+        static void Main(string[] args)
+        {
+
+            for(int i=1; i<=12; i++)
+            {
+                for(int j=1; j<=12; j++)
+                {
+                    Console.Write(i * j + " ");
+                }
+                Console.Write("\n");
+            }
+            Console.Read();
+        }
+    }
+}
