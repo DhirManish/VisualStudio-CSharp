87aca3e09f0ef5ae2c2315be7000b50be418ef33
DhirManish <manish.dhir4@gmail.com>
2018-01-29 23:27:49 +0530

    Print even fibbonacci number

---
+++ C sharp Console Application/Project Euler/EvenFibbonacci.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            int a = 0;
+            int b = 1;
+            int c = 0;
+
+            Console.Write("Enter the digit count limitator : ");
+
+            int limit = Int32.Parse(Console.ReadLine());
+
+            Console.Write(a + " ");
+
+            Console.Write(b + " ");
+
+            for (int i = 1; i <= limit; i++)
+            {
+
+                c = a + b;
+
+                if(c % 2 == 0)
+                {
+
+                    Console.Write(c + " ");
+
+                }
+
+                a = b;
+
+                b = c;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
