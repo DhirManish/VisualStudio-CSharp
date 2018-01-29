39172128b758f535f471510058cd3c7aafa2a475
DhirManish <manish.dhir4@gmail.com>
2018-01-29 23:16:47 +0530

    Print multiple number of 3 and 5 in the list of 100 PE0001

---
+++ C sharp Console Application/Project Euler/Multiple3_5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for(int i = 0; i<=100; i++)
+            {
+                if (i % 3 == 0 & i % 5 == 0 )
+                {
+                    Console.Write(i);
+                    Console.Write("\n");
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
