d6edfe6f513fb97937c0ef15a7d005a6bd33a7d4
DhirManish <manish.dhir4@gmail.com>
2018-01-29 23:02:56 +0530

    Let's start

---
+++ C sharp Console Application/ContinueCode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class ContinueCode
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("It's been a long time, let's Continue...");
+            Console.ReadLine();
+        }
+    }
+}
