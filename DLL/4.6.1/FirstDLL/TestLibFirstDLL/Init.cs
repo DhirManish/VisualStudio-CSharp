f048a2241b3a0b9cb75474613fd4f1410218d7bf
DhirManish <manish.dhir4@gmail.com>
2018-05-29 01:25:40 +0530

    Console code to use the DLL code and print the specific value

---
+++ DLL/4.6.1/FirstDLL/TestLibFirstDLL/Init.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace May29TestLibFirstDLL.TestLibFirstDLL
+{
+
+    using May29FirstDLL.FirstDLL;
+
+    public class Init
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Sum.Add(10, 10));
+            Console.ReadKey();
+        }
+    }
+}
