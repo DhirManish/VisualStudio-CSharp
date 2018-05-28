5379c82fdbe07a649a266d9dd3cbb2d619b1ad34
DhirManish <manish.dhir4@gmail.com>
2018-05-29 01:24:27 +0530

    Add method with parameters to process integer input values

---
+++ DLL/4.6.1/FirstDLL/FirstDLL/Sum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace May29FirstDLL.FirstDLL
+{
+
+    public class Sum
+    {
+
+        public static int Add(int a, int b)
+        {
+
+            return a + b;
+
+        }
+
+    }
+}
